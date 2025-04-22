58a1b350e44a64703b8486a853df150739de2268
rakibul58 <rhrahi14@gmail.com>
2025-04-22 12:10:26 +0600

    testing tables

--- src/components/UserTable/UserTable.test.tsx
+++ src/components/UserTable/UserTable.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import UserTable from "./UserTable";
 import { users } from "../../App";
@@ -6,13 +6,19 @@ import { users } from "../../App";
 describe("UserTable()", () => {
   it("should render the welcome message after 5ms", async () => {
     render(<UserTable users={users} />);
-    const welcomeMessage = await screen.findByTestId(
-      "welcome",
-      {},
-      {
-        timeout: 2000,
-      }
-    );
-    expect(welcomeMessage).toBeInTheDocument();
+    //     const welcomeMessage = await screen.findByTestId(
+    //       "welcome",
+    //       {},
+    //       {
+    //         timeout: 2000,
+    //       }
+    //     );
+    //     expect(welcomeMessage).toBeInTheDocument();
+    //     screen.logTestingPlaygroundURL();
+    //     const rows = screen.getAllByRole("row");
+    //     expect(rows).toHaveLength(users.length + 1);
+
+    const rows = within(screen.getByTestId("users")).getAllByRole("row");
+    expect(rows).toHaveLength(users.length);
   });
 });
